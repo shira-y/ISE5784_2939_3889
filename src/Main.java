8db8c8a6c6b384e1ba4e49eff3f68b53a04ff884
shira yaish <syaish@g.jct.ac.il>
2024-05-14 12:12:09 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Word");
+		System.out.println("Hello Word shira and yael");
 	}
 
 }
