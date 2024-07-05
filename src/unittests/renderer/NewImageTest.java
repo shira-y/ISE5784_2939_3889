a6cbff36aa4c3aec9e4ae10d90968f3d55bd6375
shira yaish <syaish@g.jct.ac.il>
2024-07-05 15:53:42 +0300

    finished 7.2

--- src/unittests/renderer/NewImageTest.java
+++ src/unittests/renderer/NewImageTest.java
@@ -1,131 +1,49 @@
+
 package unittests.renderer;
 
-import static java.awt.Color.*;
 
 import org.junit.jupiter.api.Test;
 
-import geometries.Sphere;
-import geometries.Triangle;
-import lighting.AmbientLight;
-import lighting.SpotLight;
+import geometries.*;
+import lighting.*;
 import primitives.*;
 import renderer.*;
 import scene.Scene;
 
-/** Test class for creating an image of ice cream with one triangle and three spheres */
+/**
+ * Test class for creating an our image with 3 objects and new effects
+ * 
+ */
 public class NewImageTest {
 
-    /** Scene for the test */
-    private final Scene scene = new Scene("Ice Cream Scene");
+	/** Scene for the test */
+	private final Scene scene = new Scene("our scane test");
 
-    /** Camera builder for the test */
-    private final Camera.Builder cameraBuilder = Camera.getBuilder()
-            .setDirection(new Vector(0, 0, -1), new Vector(0, 1, 0))
-            .setRayTracer(new SimpleRayTracer(scene));
+	/** Camera builder for the test */
+	private final Camera.Builder cameraBuilder = Camera.getBuilder()
+			.setDirection(new Vector(0, 0, -1), new Vector(0, 1, 0)).setRayTracer(new SimpleRayTracer(scene));
 
-    /** Test method to create an image of ice cream */
-    @Test
-    public void createIceCreamImage() {
-        scene.setBackground(new Color(75, 127, 190))
-                .setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)));
+	@Test
+    public void trianglesTransparentSphere() {
+    
+       scene.geometries.add(
+                            new Triangle(new Point(-150, -150, -115), new Point(150, -150, -135),
+                                         new Point(75, 75, -150))
+                               .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)),
+                            new Triangle(new Point(-150, -150, -115), new Point(-70, 70, -140), new Point(75, 75, -150))
+                               .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)),
+                            new Sphere(new Point(0, 0, -50), 40d)
+                               .setEmission(new Color(0, 0, 100)).setMaterial(new Material().setKd(0.2).setKs(0.2)
+                               .setShininess(15).setKt(0.6)));
 
-        // Adding the cone (triangle) and flipping it
-        scene.geometries.add(
-                new Triangle(new Point(-50, -50, -1000), new Point(50, -50, -1000), new Point(0, -100, -1000))
-                        .setEmission(new Color(ORANGE))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(300))
-        );
+     	
+		scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(60, 50, 0), new Vector(-1, -1, -4))
+		    	.setKl(4E-5).setKq(2E-7));
+		scene.lights.add(new PointLight(new Color(0, 0, 800), new Point(-80, 80, 0)).setKl(0.00001).setKq(0.000005));
+		Camera camera = cameraBuilder.setLocation(new Point(0, 0, 1000)).setVpDistance(1000).setVpSize(200, 200)
+				.setImageWriter(new ImageWriter("our image", 500, 500)).build();
 
-        // Adding the ice cream scoops (spheres) with adjusted positions and properties
-        scene.geometries.add(
-                new Sphere(new Point(0, -30, -1000), 30) // First scoop
-                        .setEmission(new Color(PINK))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.1).setShininess(30).setKt(0.1)),
-                new Sphere(new Point(-35, -30, -1000), 30) // Second scoop
-                        .setEmission(new Color(CYAN))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.3).setShininess(50).setKt(0.1)),
-                new Sphere(new Point(35, -30, -1000), 30) // Third scoop
-                        .setEmission(new Color(MAGENTA))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.3).setShininess(50).setKt(0.1))
-        );
-
-        // Adding a light source
-        scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(-100, -100, -500), new Vector(1, 1, -2))
-                .setKl(4E-5).setKq(2E-7));
-
-        Camera camera1 = cameraBuilder.setLocation(new Point(0, 0, 1000))
-                .setVpDistance(1000)
-                .setVpSize(150, 150)
-                .setImageWriter(new ImageWriter("iceCreamImage", 600, 600))
-                .build();
-
-        camera1.renderImage();
-        camera1.writeToImage();
-    }
+		camera.renderImage();
+		camera.writeToImage();
+	}
 }
-
-
-
-
-
-
-
-//package unittests.renderer;
-//
-//import static java.awt.Color.*;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import geometries.Sphere;
-//import geometries.Triangle;
-//import lighting.AmbientLight;
-//import lighting.SpotLight;
-//import primitives.*;
-//import renderer.*;
-//import scene.Scene;
-//
-///** Test class for creating a new image with specified geometries and lighting */
-//public class NewImageTest {
-//
-//    /** Scene for the test */
-//    private final Scene scene = new Scene("Test Scene");
-//
-//    /** Camera builder for the test */
-//    private final Camera.Builder cameraBuilder = Camera.getBuilder()
-//            .setDirection(new Vector(0, 0, -1), new Vector(0, 1, 0))
-//            .setRayTracer(new SimpleRayTracer(scene));
-//
-//    /** Test method to create a new image */
-//    @Test
-//    public void createNewImage() {
-//        scene.setBackground(new Color(75, 127, 190))
-//                .setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)));
-//
-//        scene.geometries.add(
-//                new Sphere(new Point(0, 0, -1000), 50)
-//                        .setEmission(new Color(RED))
-//                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100).setKt(0.3)),
-//                new Sphere(new Point(50, 0, -1050), 50)
-//                        .setEmission(new Color(GREEN))
-//                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100).setKt(0.4)),
-//                new Triangle(new Point(-50, -50, -1000), new Point(-50, 50, -1000), new Point(0, 0, -1000))
-//                        .setEmission(new Color(BLUE))
-//                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(300)),
-//                new Triangle(new Point(-50, -50, -1000), new Point(50, -50, -1000), new Point(0, -100, -1000))
-//                        .setEmission(new Color(YELLOW))
-//                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(300))
-//        );
-//
-//        scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(-100, -100, -500), new Vector(1, 1, -2))
-//                .setKl(4E-5).setKq(2E-7));
-//
-//        Camera camera1 = cameraBuilder.setLocation(new Point(0, 0, 1000))
-//                .setVpDistance(1000)
-//                .setVpSize(150, 150)
-//                .setImageWriter(new ImageWriter("newImage", 600, 600))
-//                .build();
-//
-//        camera1.renderImage();
-//        camera1.writeToImage();
-//    }
-//}
